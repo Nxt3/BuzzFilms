3a3ab6d7cd1cdef845ec2ed3b7e880e28f43eb6b
nxt3 <nategantt@gmail.com>
2016-02-02 00:07:01 -0500

    Changed app colors to cyan/yellow
    Removed unnecessary color resources from colors.xml

--- app/src/main/java/com/nullpointexecutioners/buzzfilms/LoginActivity.java
+++ app/src/main/java/com/nullpointexecutioners/buzzfilms/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.nullpointexecutioners.buzzfilms;
 
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -15,5 +16,8 @@ public class LoginActivity extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // TODO: change the toolbar title color non-programmatically?
+        toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white));
     }
 }
