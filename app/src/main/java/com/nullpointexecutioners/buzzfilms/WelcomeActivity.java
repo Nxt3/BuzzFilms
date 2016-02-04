7accf19023803145d300f8932aff44caf2f7bf37
nxt3 <nategantt@gmail.com>
2016-02-04 18:09:05 -0500

    Removed LoginActivity in favor of WelcomeActivity

--- app/src/main/java/com/nullpointexecutioners/buzzfilms/WelcomeActivity.java
+++ app/src/main/java/com/nullpointexecutioners/buzzfilms/WelcomeActivity.java
@@ -3,6 +3,7 @@ package com.nullpointexecutioners.buzzfilms;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -15,6 +16,9 @@ public class WelcomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // TODO: change the toolbar title color non-programmatically?
+        toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white));
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
