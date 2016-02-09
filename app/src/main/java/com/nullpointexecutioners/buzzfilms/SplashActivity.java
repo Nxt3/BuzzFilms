5aa6f663b1172a37dc616d7b464bd84e910aea4b
nxt3 <nategantt@gmail.com>
2016-02-09 12:55:54 -0500

    Working towards reworking how the Splash screen is displayed
    Added new colors for text on light/dark backgrounds
    
    Changed layout of Login screen
    
    Converted hardcoded strings to String resources

--- app/src/main/java/com/nullpointexecutioners/buzzfilms/SplashActivity.java
+++ app/src/main/java/com/nullpointexecutioners/buzzfilms/SplashActivity.java
@@ -1,17 +1,49 @@
 package com.nullpointexecutioners.buzzfilms;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
-public class SplashActivity extends AppCompatActivity {
+//public class SplashActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        Intent intent = new Intent(this, WelcomeActivity.class);
+//        startActivity(intent);
+//        finish();
+//    }
+//}
+
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
 
-        Intent intent = new Intent(this, WelcomeActivity.class);
-        startActivity(intent);
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
         finish();
     }
+
 }
