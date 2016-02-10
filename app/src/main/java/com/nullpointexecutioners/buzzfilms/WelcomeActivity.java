4eab206e0409991faa96d88a8578d607ecf76db9
nxt3 <nategantt@gmail.com>
2016-02-10 14:53:44 -0500

    Lots of cleanup
    Imported ButterKnife library (super awesome)
    
    Imported MaterialDialogs library
    
    Deleted LoginActivity and replaced the Login flow with a dialog
    
    Removed support toolbar code from the WelcomeActivity

--- app/src/main/java/com/nullpointexecutioners/buzzfilms/WelcomeActivity.java
+++ app/src/main/java/com/nullpointexecutioners/buzzfilms/WelcomeActivity.java
@@ -2,13 +2,22 @@ package com.nullpointexecutioners.buzzfilms;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.afollestad.materialdialogs.Theme;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -16,32 +25,96 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        ButterKnife.bind(this);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        if (toolbar != null) {
-            setSupportActionBar(toolbar);
-            // TODO: change the toolbar title color non-programmatically?
-            toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.primary_text_dark));
-        }
-
-        Button loginButton = (Button) findViewById(R.id.btnLogin);
-        Button registerButton = (Button) findViewById(R.id.btnRegister);
-
-        // Capture button clicks
-        loginButton.setOnClickListener(new OnClickListener() {
-            public void onClick(View arg0) {
-                // Start LoginActivity.class
-                // TODO: instead of calling a new activity--maybe move this into a popup dialog?
-                Intent myIntent = new Intent(WelcomeActivity.this,
-                        LoginActivity.class);
-                startActivity(myIntent);
-            }
-        });
-        registerButton.setOnClickListener(new OnClickListener() {
+        /*Handle the Register button*/
+        findViewById(R.id.btnRegister).setOnClickListener(new OnClickListener() {
             public void onClick(View arg0) {
 //                placeholder for register button--not yet...
                 Snackbar.make(findViewById(android.R.id.content), "Not yet ;)", Snackbar.LENGTH_LONG).show();
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ButterKnife.unbind(this);
+    }
+
+    @OnClick(R.id.btnLogin)
+    public void showLoginDialog() {
+        MaterialDialog dialog = new MaterialDialog.Builder(WelcomeActivity.this)
+                .title(getString(R.string.login_dialog_title))
+                .customView(R.layout.login_dialog, true)
+                .theme(Theme.DARK)
+                .positiveText(getString(R.string.title_activity_login))
+                .negativeText(getString(R.string.cancel))
+                .onPositive(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                        final EditText loginUsernameInput =  (EditText) dialog.getCustomView().findViewById(R.id.login_username);
+                        final EditText loginPasswordInput = (EditText) dialog.getCustomView().findViewById(R.id.login_password);
+                        String username = loginUsernameInput.getText().toString();
+                        String password = loginPasswordInput.getText().toString();
+
+                        /*Check and see if the Login fields are blank*/
+                        boolean emptyFields = true;
+                        if (username.length() != 0 && password.length() != 0) {
+                            emptyFields = false;
+                        }
+                        if (username.equals("user") && password.equals("pass") && !emptyFields) {
+                            // Login works - proceed to application
+                            Intent loginIntent = new Intent(WelcomeActivity.this, MainActivity.class);
+                            startActivity(loginIntent);
+                        } else {
+                            //TODO, generate another dialog stating the login was unsuccessful
+                        }
+                    }
+                })
+                .onNegative(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                        dialog.dismiss(); //close the dialog since they pressed "Cancel"
+                    }
+                }).build();
+
+        final View loginAction;
+        loginAction = dialog.getActionButton(DialogAction.POSITIVE);
+        final EditText loginPasswordInput = (EditText) dialog.getCustomView().findViewById(R.id.login_password);
+        //noinspection ConstantConditions
+        loginPasswordInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                loginAction.setEnabled(s.toString().trim().length() > 0);
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        dialog.show();
+        loginAction.setEnabled(false); //disabled by default
+    }
+
+    /**
+     * Helper method for creating custom Snackbars
+     * @param layout view to place the Snackbar in
+     * @param text what you want the Snackbar to say
+     * @param duration how long you want the Snackbar to appear for
+     * @param backgroundColor color you want the Snackbar's background to be
+     * @param textColor color you want the Snackbar's text to be
+     * @return the custom made Snackbar
+     */
+    @NonNull
+    public static Snackbar makeSnackbar(@NonNull View layout, @NonNull CharSequence  text, int duration, int backgroundColor, int textColor/*, int actionTextColor*/){
+        Snackbar snackBarView = Snackbar.make(layout, text, duration);
+        snackBarView.getView().setBackgroundColor(backgroundColor);
+        //snackBarView.setActionTextColor(actionTextColor);
+        TextView tv = (TextView) snackBarView.getView().findViewById(android.support.design.R.id.snackbar_text);
+        tv.setTextColor(textColor);
+        return snackBarView;
     }
 }
