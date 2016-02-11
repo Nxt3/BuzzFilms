7aec4845751f52e0a5816af5f818e1bde531fa26
Matthew Lewine <mlewine3@gatech.edu>
2016-02-11 17:44:45 -0500

    Add email and name to User class

--- app/src/main/java/com/nullpointexecutioners/buzzfilms/User.java
+++ app/src/main/java/com/nullpointexecutioners/buzzfilms/User.java
@@ -7,10 +7,14 @@ package com.nullpointexecutioners.buzzfilms;
 public class User {
     private String username;
     private String password; // Not hashed yet
+    private String name;
+    private String email;
 
-    public User(String username, String password) {
+    public User(String username, String password, String name, String email) {
         this.username = username;
         this.password = password;
+        this.name = name;
+        this.email = email;
     }
 
     public String getUsername() {
@@ -22,12 +26,28 @@ public class User {
         return this.password;
     }
 
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
     public void setUsername(String username) {
         this.username = username;
     }
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
 }
