bba9d7eb2512e558427301cee52663b60a2e54e4
Matthew Lewine <mlewine3@gatech.edu>
2016-02-15 13:31:25 -0500

    Add Major and Interests to the User class

--- app/src/main/java/com/nullpointexecutioners/buzzfilms/User.java
+++ app/src/main/java/com/nullpointexecutioners/buzzfilms/User.java
@@ -1,5 +1,9 @@
 package com.nullpointexecutioners.buzzfilms;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Matthew on 2/11/2016.
  * This is the user class.
@@ -9,12 +13,18 @@ public class User {
     private String password; // Not hashed yet
     private String name;
     private String email;
+    Major studentMajor;
+    private String interests;
 
     public User(String username, String password, String name, String email) {
         this.username = username;
         this.password = password;
         this.name = name;
         this.email = email;
+    }
+
+    public enum Major {
+        CS, EE, ME, ISYE, Math, Phys, Chem, ChemE
     }
 
     public String getUsername() {
@@ -34,6 +44,12 @@ public class User {
         return this.email;
     }
 
+    public Major getMajor() { return this.studentMajor; }
+
+    public String getInterests() {
+        return interests;
+    }
+
     public void setUsername(String username) {
         this.username = username;
     }
@@ -46,8 +62,16 @@ public class User {
         this.name = name;
     }
 
+
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public void setMajor(Major major) { this.studentMajor = major; }
+
+    public void setInterests(String interests) {
+        this.interests = interests;
+    }
+
 
 }
