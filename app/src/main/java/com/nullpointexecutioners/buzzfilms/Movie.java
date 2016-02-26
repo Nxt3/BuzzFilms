3482b5be5cc8e272b2fb7153b11f9225f725cab9
Matthew Lewine <mlewine3@gatech.edu>
2016-02-26 13:40:09 -0500

    Create the Movie class

---
+++ app/src/main/java/com/nullpointexecutioners/buzzfilms/Movie.java
@@ -0,0 +1,41 @@
+package com.nullpointexecutioners.buzzfilms;
+
+import android.text.TextUtils;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Matthew on 2/26/2016.
+ */
+public class Movie {
+    private String title;
+    private int year;
+    private String synopsis;
+    private String posterUrl;
+    private int criticsScore;
+    private ArrayList<String> castList;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+
+    public int getCriticsScore() {
+        return criticsScore;
+    }
+
+    public String getCastList() {
+        return TextUtils.join(", ", castList);
+    }
+}
