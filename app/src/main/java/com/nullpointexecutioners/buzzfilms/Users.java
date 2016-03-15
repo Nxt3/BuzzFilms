4b58e00478bc462a1a21e787e6af72b674f22b33
nxt3 <nategantt@gmail.com>
2016-03-14 22:51:10 -0400

    Javadoc'd Users class

--- app/src/main/java/com/nullpointexecutioners/buzzfilms/Users.java
+++ app/src/main/java/com/nullpointexecutioners/buzzfilms/Users.java
@@ -8,6 +8,14 @@ public class Users {
     private String major;
     private String status;
 
+    /**
+     * Constructor for Users
+     * @param username of user
+     * @param name of user
+     * @param email of user
+     * @param major of user
+     * @param status of user's account
+     */
     public Users(String username, String name, String email, String major, String status) {
         this.username = username;
         this.name = name;
@@ -16,22 +24,42 @@ public class Users {
         this.status = status;
     }
 
+    /**
+     * Gets this user's username
+     * @return username of user
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Gets this user's name
+     * @return name of user
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets this user's email
+     * @return email of user
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Gets this user's major
+     * @return major of user
+     */
     public String getMajor() {
         return major;
     }
 
+    /**
+     * Gets the account status of this user
+     * @return account status of user
+     */
     public String getStatus() {
         return status;
     }
